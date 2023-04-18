a2cfc06b6586ab434df893c585e65f8e3e75bed2
Vianbar <ivanero1102@gmail.com>
2023-04-18 17:00:47 +0200

    TrabajoBack_Dante

--- DAO/operativaPunto.php
+++ DAO/operativaPunto.php
@@ -3,6 +3,20 @@ include_once("/xampp/htdocs/Final-Proyect/TO/punto.php");
 include_once("/xampp/htdocs/Final-Proyect/DS/crud.php");
 
 class OperativaPunto{
+        /* La funcion recibe un objetpo de tipo punto con los puntos que se van a sumar, ejecuta la rest de los puntos y actualiza la BBDD */
+
+    /*La funcion no devuelve un mensaje en caso de que todo haya funcionado y un mensaje de error en caso contrario*/
+    public function sumaPuntos($punto){
+    
+    }
+
+
+    /* La funcion recibe un objetpo de tipo punto con los puntos que se van a restar, ejecuta la rest de los puntos y actualiza la BBDD */
+
+    /*La funcion no devuelve un mensaje en caso de que todo haya funcionado y un mensaje de error en caso contrario*/
+    public function restaPuntos($punto, $puntos){
+
+    }
 
 }
 
