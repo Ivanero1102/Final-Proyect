4f663637d5c48ff14a1cd216c04a450f398fb184
WenLeon <79409441+WenLeon@users.noreply.github.com>
2023-04-23 19:55:44 +0200

    Cambios Dan y Wen

--- DAO/operativaPunto.php
+++ DAO/operativaPunto.php
@@ -2,20 +2,34 @@
 include_once("/xampp/htdocs/Final-Proyect/TO/punto.php");
 include_once("/xampp/htdocs/Final-Proyect/DS/crud.php");
 
-class OperativaPunto{
-        /* La funcion recibe un objetpo de tipo punto con los puntos que se van a sumar, ejecuta la rest de los puntos y actualiza la BBDD */
+class OperativaPunto {
+      
+    public function sumaPuntos($punto, $puntos) { // MIRANDO VIDEOS (+puntosActuales, +totalPuntos)
 
-    /*La funcion no devuelve un mensaje en caso de que todo haya funcionado y un mensaje de error en caso contrario*/
-    public function sumaPuntos($punto){
-    
+        // Actualización de los puntos en el objeto $punto
+        $punto->__set('puntosActuales', ($punto->__get('puntosActuales') + $puntos));
+        $punto->__set('totalPuntos', ($punto->__get('totalPuntos') + $puntos));
+
+        // Actualización de puntos en la base de datos
+        $conn = (new CRUD())->consultaPreparada("UPDATE puntos SET puntos_actuales=:puntos_actuales, total_puntos=:total_puntos WHERE id_usuario=:id_usuario", array(':puntos_actuales' => $punto->get('puntosActuales'), ':total_puntos' => $punto->get('totalPuntos'), ':id_usuario' => $punto->get('idUsuario')));
+        return true;
     }
 
+    /* La funcion recibe un objeto de tipo punto con los puntos que se van a restar, ejecuta la resta de los puntos y actualiza la BBDD */
+    /* La funcion no devuelve un mensaje en caso de que todo haya funcionado y un mensaje de error en caso contrario */
+    public function restaPuntos($punto, $puntos) { // DONANDO PUNTOS (+puntosGastados, -puntosActuales)
 
-    /* La funcion recibe un objetpo de tipo punto con los puntos que se van a restar, ejecuta la rest de los puntos y actualiza la BBDD */
+        // Actualización de los puntos en el objeto $punto
+        if ($punto->__get('puntosActuales') >= $punto->__get('puntosGastados')) { // Si tiene los puntos suficientes...
+            $punto->__set('puntosGastados', ($punto->__get('puntosGastados') + $puntos));
+            $punto->__set('puntosActuales', ($punto->__get('puntosActuales') - $puntos));
 
-    /*La funcion no devuelve un mensaje en caso de que todo haya funcionado y un mensaje de error en caso contrario*/
-    public function restaPuntos($punto, $puntos){
-
+            // Actualización de puntos en la base de datos
+            $conn = (new CRUD())->consultaPreparada("UPDATE puntos SET puntos_actuales=:puntos_actuales, puntos_gastados=:puntos_gastados WHERE id_usuario=:id_usuario", array(':puntos_actuales' => $punto->get('puntosActuales'), ':puntos_gastados' => $punto->get('puntosGastados'), ':id_usuario' => $punto->get('idUsuario')));
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
--- DAO/operativaUsuario.php
+++ DAO/operativaUsuario.php
@@ -2,33 +2,104 @@
 include_once("/xampp/htdocs/Final-Proyect/TO/usuario.php");
 include_once("/xampp/htdocs/Final-Proyect/DS/crud.php");
 
-class OperativaUsuaio{
+class OperativaUsuaio
+{
 
-    
-    /* La funcion recibe un objetpo de tipo usuario, con todos los datos que posee un usuario, y se encarga de introducirlo en la base de datos*/
-
-    /*La funcion no devuelve un mensaje en caso de que todo haya funcionado y un mensaje de error en caso contrario*/
-    public function registro($usuario){
-    
+    public function __construct()
+    {
     }
 
+
+    /* La funcion recibe un objeto de tipo usuario, con todos los datos que posee un usuario, y se encarga de introducirlo en la base de datos*/
+    /*La funcion no devuelve un mensaje en caso de que todo haya funcionado y un mensaje de error en caso contrario*/
+    public function registro($usuario)
+    {
+        try {
+            
+            // Atributos del usuario
+            $idUsuario = $usuario->__get('idUsuario');
+            $nombre_usuario     = $usuario->__get('nombre_usuario');
+            $apellidos_usuario = $usuario->__get('apellidos_usuario');
+            $edad_usuario = $usuario->__get('edad_usuario');
+            $telefono_usuario = $usuario->__get('telefono_usuario');
+            $contrasegna_usuario = $usuario->__get('contrasegna_usuario');
+            
+            //Cifrar la contrasena 
+            $password_cifrada = password_hash($contrasegna_usuario, PASSWORD_DEFAULT);
+
+            // Consulta para comprobar si el dato ya existe en la tabla
+            $crud = new CRUD();
+            $sql = "SELECT COUNT(*) as total FROM USUARIOS WHERE id_usuario = :id_usuario";
+            $resultado = $crud->consultaPreparada($sql, array(':id_usuario' => $idUsuario));
+
+            // Si el resultado es mayor a 0, significa que el dato ya existe
+            if ($resultado['total'] > 0) {
+                return false;
+            } else {
+                //Insertar el usuario 
+                $crud = new CRUD();
+                $sql = "INSERT INTO USUARIOS (nombre_usuario, apellidos_usuario, edad_usuario, telefono_usuario, contrasegna_usuario) VALUES (:nombre_usuario, :apellidos_usuario, :edad_usuario, :telefono_usuario, :contrasegna_usuario)";
+                $crud->consultaPreparada($sql, array(':nombre_usuario' => $nombre_usuario, ':apellidos_usuario' => $apellidos_usuario, ':edad_usuario'=> $edad_usuario, ':telefono_usuario'=>$telefono_usuario,':contrasegna_usuario'=>$password_cifrada));
+
+                return true;
+            }
+        } catch (PDOException $e) {
+            echo "Error " . $e->getMessage();
+            die();
+        }
+    }
 
     /* La funcion recibe un objetpo de tipo usuario, con el nombre del usuario y su contraseña, comprueba que ambos coincidan con las que se encuentran en la BBDD 
         , importante desencriptar la contraseña de la BBDD antes de comparar.
         En caso de que todo coincida, se creara una sesion llamada $_SESSION['usuario'] */
-
     /*La funcion no devuelve nada*/
-    public function login($nombreUsuario, $contrasenaUsuario){
+    public function login($usuario)
+    {
 
+        try {
+
+            
+            // Atributos del usuario
+            $idUsuario = $usuario->__get('idUsuario');
+            $nombre_usuario     = $usuario->__get('nombre_usuario');
+            $contrasegna_usuario = $usuario->__get('contrasegna_usuario');
+
+            // Conexion bbdd 
+            $crud = new CRUD();
+            $sql = "SELECT COUNT(*) as total FROM USUARIOS WHERE id_usuario = :id_usuario";
+            $resultado = $crud->consultaPreparada($sql, array(':id_usuario' => $idUsuario));
+
+            if ($resultado['contrasegna_usuario'] != null) {
+
+                //password_verify(string $password, string $hash)
+                if (password_verify($contrasegna_usuario, $resultado['contrasegna_usuario'])) {
+                    // session_start();
+                    $_SESSION['usuario'] = $nombre_usuario;
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            die("Error :" . $e->getMessage());
+        }
     }
 
 
     /* La funcion se encargara de borrar la sesion $_SESSION['usuario'] */
 
     /*La funcion no devuelve nada*/
-    public function logout(){
-    
+    public function logout()
+    {
+        try {
+            session_unset(); // Eliminar todas las variables de sesión
+            session_destroy(); // Destruir la sesión actual
+
+        } catch (PDOException $e) {
+            die("Error: " . $e->getMessage());
+        }
     }
 }
 
-?>
